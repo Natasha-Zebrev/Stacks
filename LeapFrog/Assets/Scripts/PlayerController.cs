e7cfaff03e9bd33141ec186e38fb0ec7d1ba23e7
Richard Lussier <lussi036@morris.umn.edu>
2022-03-25 12:02:13 -0500

    Add Walking Animation
    Added the walking animation for the main character and edited the code so that the animation only plays when the character is on the ground and is moving left or right. Also increased the linear drag so the character doesn't slide around and increased the jump height to compensate for the lost velocity.
    
    Co-authored-by: day00090 <day00090@morris.umn.edu>

--- LeapFrog/Assets/Scripts/PlayerController.cs
+++ LeapFrog/Assets/Scripts/PlayerController.cs
@@ -4,15 +4,18 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-
     [SerializeField] private Rigidbody2D mainRigidBody;
     [SerializeField] private Transform playerTransform;
     [SerializeField] private SpriteRenderer mainSpriteRenderer;
     [SerializeField] private float moveSpeed;
     [SerializeField] private float jumpHeight;
     bool isGrounded = true;
+    [SerializeField] private Animator anim;
 
-
+    private void Start()
+    {
+        anim.enabled = false;
+    }
     // Update is called once per frame
     void Update()
     {
@@ -20,19 +23,28 @@ public class PlayerController : MonoBehaviour
         {
             mainRigidBody.AddForce(new Vector2(-moveSpeed * Time.deltaTime, 0));
             mainSpriteRenderer.flipX = true;
+            anim.enabled = true && isGrounded;
         }
 
         if (Input.GetKey(KeyCode.D))
         {
             mainRigidBody.AddForce(new Vector2(moveSpeed * Time.deltaTime, 0));
             mainSpriteRenderer.flipX = false;
+            anim.enabled = true && isGrounded;
         }
 
         if ((Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.Space)) && isGrounded==true)
         {
             mainRigidBody.AddForce(new Vector2(0 , jumpHeight));
             isGrounded = false;
+            anim.enabled = false;
         }
+
+        if(Mathf.Abs(mainRigidBody.velocity.x) == 0)
+        {
+            anim.enabled = false;
+        }
+
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
