d4e7c53d6f35655a14bb05b3989d141413930604
Richard Lussier <lussi036@morris.umn.edu>
2022-03-30 13:05:16 -0500

    Fix enemies facing correct direction
    Modified the code in FollowPlayer.cs to correctly face the player while going after them. This also includes adding the VectorSwizzleExtensions.cs script to help modify vectors.
    
    Co-authored-by: day00090 <day00090@morris.umn.edu>

--- LeapFrog/Assets/Scripts/FollowPlayer.cs
+++ LeapFrog/Assets/Scripts/FollowPlayer.cs
@@ -7,21 +7,40 @@ public class FollowPlayer : MonoBehaviour
 
     [SerializeField] private Transform mainTransform;
     [SerializeField] private int moveSpeed;
+    private bool playerToLeft;
     void Update()
     {
+        playerToLeft = PlayerController.instance.transform.position.x < mainTransform.position.x;
         moveToPlayer();
     }
 
     private void moveToPlayer()
     {
-        Vector3 playerPos = PlayerController.instance.transform.position;
-        //mainTransform.LookAt(playerPos);
 
-        Vector3 currentRotation = mainTransform.rotation.eulerAngles;
-        currentRotation.x = 0;
-        mainTransform.eulerAngles = currentRotation;
+        faceCorrectDirection(playerToLeft);
+
+        Vector3 playerPos = PlayerController.instance.transform.position;
 
         Vector3 directionToPlayer = (playerPos - mainTransform.position).normalized;
-        mainTransform.position += (directionToPlayer * moveSpeed * Time.deltaTime);
+        mainTransform.position += (directionToPlayer * moveSpeed * Time.deltaTime).SetY(0);
+    }
+
+    private void faceCorrectDirection(bool playerLeft)
+    {
+        if(playerLeft)
+        {
+            correctDirection(0);
+        }
+        else if(!playerLeft)
+        {
+            correctDirection(180);
+        }
+    }
+
+    private void correctDirection(int rotateX)
+    {
+        Vector3 currentRotation = mainTransform.rotation.eulerAngles;
+        currentRotation.y = rotateX;
+        mainTransform.eulerAngles = currentRotation;
     }
 }
