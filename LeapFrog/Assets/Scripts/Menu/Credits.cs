43f93a0f32e575116de3f0658e9f8dc67d59e68e
Kyle Day <day00090@morris.umn.edu>
2022-03-31 10:23:30 -0500

    Add Credits to Credits Scene
    All members of the development have been credited in alphabetical order, just as all art brought into the project thus far has been credited to their respective authors.

---
+++ LeapFrog/Assets/Scripts/Menu/Credits.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Credits : MonoBehaviour
+{
+    [SerializeField] private Button back;
+    // Start is called before the first frame update
+    void Start()
+    {
+        back.onClick.AddListener(() => LoadingScreen.LoadScene("MainMenu"));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
