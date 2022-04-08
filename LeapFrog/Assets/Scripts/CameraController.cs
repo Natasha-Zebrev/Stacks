09280a4b91440cf2d2998600f01757f92378ebdb
Richard Lussier <lussi036@morris.umn.edu>
2022-04-08 12:53:42 -0500

    Add camera scroll
    If you use the scroll wheel you can now zoom in and out. There is currently a bug where you can zoom out as much as you want and therefore see below you. Alternatively, you can zoom in super far and flip the world upside down. This will be fixed later.
    
    Co-authored-by: day00090 <day00090@morris.umn.edu>

--- LeapFrog/Assets/Scripts/CameraController.cs
+++ LeapFrog/Assets/Scripts/CameraController.cs
@@ -5,16 +5,35 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-    [SerializeField] private Transform mainTransform;
+    [SerializeField] private Camera mainCamera;
+    private Transform mainTransform;
     [SerializeField] private Transform target;
     [SerializeField] private float minY;
+    private Vector3 currentPos;
+
+    void Start()
+    {
+        mainTransform = mainCamera.transform;
+        currentPos = mainTransform.position;
+        currentPos.y = Math.Max(minY, target.position.y);
+    }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        Vector3 currentPos = mainTransform.position;
         currentPos.x = target.position.x;
-        currentPos.y = Math.Max(minY, target.position.y);
         mainTransform.position = currentPos;
+        Vector3 stageDimensions = mainCamera.ScreenToWorldPoint(new Vector3(0, 0, 0));
+        Debug.Log(stageDimensions.y);
+        if (Input.mouseScrollDelta.y > 0)
+            mainCamera.orthographicSize -= 2;
+
+        else if (Input.mouseScrollDelta.y < 0)
+        {
+            mainCamera.orthographicSize += 2;
+            mainCamera.transform.position += mainTransform.position.SetY(currentPos.y * Mathf.Abs(stageDimensions.y));
+        }
+
+       // mainTransform.position.SetY(Mathf.Clamp(stageDimensions.y, minY, 100)); 
     }
 }
