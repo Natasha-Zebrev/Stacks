9859d0be2e42c455ee414678895ca10ad8f72c90
Richard Lussier <lussi036@morris.umn.edu>
2022-04-27 13:35:34 -0500

    Add more levels to level select
    Added additional code that allows the user to move between different level select screens. You can now click a "next" or "previous" button to go between screens. Additionally, added level9 to the build so its level button works correctly.
    
    Each button still needs art created for it, but this commit gives the bare essentially for a button to work.

--- LeapFrog/Assets/Scripts/Menu/LevelSelector.cs
+++ LeapFrog/Assets/Scripts/Menu/LevelSelector.cs
@@ -5,13 +5,91 @@ using UnityEngine.UI;
 
 public class LevelSelector : MonoBehaviour
 {
-    [SerializeField] private Button level1;
-    [SerializeField] private Button level2;
-    // Start is called before the first frame update
+    [SerializeField] private List<GameObject> levelButtons;
+    [SerializeField] private GameObject previous;
+    [SerializeField] private GameObject next;
+
+    private int previousLevels = -3;
+    private int currentLevels = 0;
+    private int nextLevels = 3;
     void Start()
     {
+        enableOnlyFirst();
+
+        next.GetComponent<Button>().onClick.AddListener(() => changeNextButton());
+        previous.GetComponent<Button>().onClick.AddListener(() => changePreviousButton());
+
         Time.timeScale = 1;
-        level1.onClick.AddListener(() => LoadingScreen.LoadScene("Level1"));
-        level2.onClick.AddListener(() => LoadingScreen.LoadScene("Level2"));
+        levelButtons[0].GetComponent<Button>().onClick.AddListener(() => LoadingScreen.LoadScene("Level1"));
+        levelButtons[1].GetComponent<Button>().onClick.AddListener(() => LoadingScreen.LoadScene("Level2"));
+        levelButtons[2].GetComponent<Button>().onClick.AddListener(() => LoadingScreen.LoadScene("Level3"));
+        levelButtons[3].GetComponent<Button>().onClick.AddListener(() => LoadingScreen.LoadScene("Level4"));
+        levelButtons[4].GetComponent<Button>().onClick.AddListener(() => LoadingScreen.LoadScene("Level5"));
+        levelButtons[5].GetComponent<Button>().onClick.AddListener(() => LoadingScreen.LoadScene("Level6"));
+        levelButtons[6].GetComponent<Button>().onClick.AddListener(() => LoadingScreen.LoadScene("Level7"));
+        levelButtons[7].GetComponent<Button>().onClick.AddListener(() => LoadingScreen.LoadScene("Level8"));
+        levelButtons[8].GetComponent<Button>().onClick.AddListener(() => LoadingScreen.LoadScene("Level9"));
+    }
+    
+    private void enableOnlyFirst()
+    {
+        for(int i = 3; i < levelButtons.Count; i++)
+            levelButtons[i].SetActive(false);
+    }
+
+    private void changeNextButton()
+    {
+        disableCurrentLevels();
+        enableNextLevels();
+        Debug.Log("Prev: " + previousLevels + "\nCurr: " + currentLevels + "\nNext: " + nextLevels);
+    }
+
+    private void changePreviousButton()
+    {
+        disableCurrentLevels();
+        enablePreviousLevels();
+        Debug.Log("Prev: " + previousLevels + "\nCurr: " + currentLevels + "\nNext: " + nextLevels);
+    }
+    private void disableCurrentLevels()
+    {
+        for (int i = currentLevels; i < nextLevels; i++)
+            levelButtons[i].SetActive(false);
+    }
+    private void enableNextLevels()
+    {
+        for (int i = nextLevels; i < nextLevels + 3; i++)
+            levelButtons[i].SetActive(true);
+        previousLevels = currentLevels;
+        currentLevels = nextLevels;
+        nextLevels += 3;
+    }
+
+    private void enablePreviousLevels()
+    {
+        for(int i = previousLevels; i < currentLevels; i++)
+            levelButtons[i].SetActive(true);
+        nextLevels = currentLevels;
+        currentLevels = previousLevels;
+        previousLevels -= 3;
+    }
+
+
+    void Update()
+    {
+        if (nextLevels >= levelButtons.Count)
+        {
+            next.SetActive(false);
+            previous.SetActive(true);
+        }
+        else if (previousLevels < 0)
+        {
+            previous.SetActive(false);
+            next.SetActive(true);
+        }
+        else
+        {
+            next.SetActive(true);
+            previous.SetActive(true);
+        }
     }
 }
